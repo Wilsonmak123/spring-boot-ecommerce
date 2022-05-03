76204743336124dc9dd74e567660cfc300cf9269
Abc69707753 <wilson_mak123@hotmail.com>
2022-05-03 20:57:28 +0800

    Edit Config class and Create Order Repository

--- src/main/java/com/loveshopping/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/loveshopping/ecommerce/config/MyDataRestConfig.java
@@ -1,11 +1,6 @@
-package com.loveshopping.ecommerce.config;
+package com.luv2code.ecommerce.config;
 
-import com.loveshopping.ecommerce.entity.Country;
-import com.loveshopping.ecommerce.entity.Product;
-import com.loveshopping.ecommerce.entity.ProductCategory;
-import javax.persistence.metamodel.EntityType;
-
-import com.loveshopping.ecommerce.entity.State;
+import com.loveshopping.ecommerce.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -22,8 +18,8 @@ import java.util.Set;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    @Value("${allowed.origins")
-    private String [] theAllowedOrigins;
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
 
     private EntityManager entityManager;
 
@@ -34,51 +30,61 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST,
-                                                HttpMethod.DELETE,HttpMethod.PATCH};
+                HttpMethod.DELETE, HttpMethod.PATCH};
 
-        // disable HTTP methods for Product: PUT, POST and DELETE
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
         disableHttpMethods(Product.class, config, theUnsupportedActions);
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
 
-        // config cors mapping
+        // configure cors mapping
         cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
         config.getExposureConfiguration()
                 .forDomainType(theClass)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
 
         // expose entity ids
-
+        //
 
         // - get a list of all entity classes from the entity manager
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
-        // create an array of the entity manager
+        // - create an array of the entity types
         List<Class> entityClasses = new ArrayList<>();
 
-        // get the entity types for the entities
-        for (EntityType tempEntityType: entities){
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
             entityClasses.add(tempEntityType.getJavaType());
         }
 
         // - expose the entity ids for the array of entity/domain types
-        Class [] domainTypes = entityClasses.toArray(new Class[0]);
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
         config.exposeIdsFor(domainTypes);
-
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
