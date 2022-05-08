3ffe27d6a3c1c6c93fa1d09d4bea6f94c14df17e
Abc69707753 <wilson_mak123@hotmail.com>
2022-05-08 14:15:58 +0800

    disable CSRF since not using cookies for session tracking

--- src/main/java/com/loveshopping/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/loveshopping/ecommerce/config/SecurityConfiguration.java
@@ -26,5 +26,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // force a non-empty response body for 401's to make the response
         Okta.configureResourceServer401ResponseBody(http);
+
+        // disable CSRF since we are not using Cookies for session tracking
+        http.csrf().disable();
     }
 }
